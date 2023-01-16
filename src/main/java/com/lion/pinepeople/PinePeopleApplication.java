386d3cf57212f57ef6cdb45653f67c161f605c1e
KimGunWoo9595 <medneter@naver.com>
2023-01-16 22:51:49 +0900

    build project

---
+++ src/main/java/com/lion/pinepeople/PinePeopleApplication.java
@@ -0,0 +1,13 @@
+package com.lion.pinepeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinePeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinePeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lion/pinepeople/PinePeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.pinepeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinePeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
