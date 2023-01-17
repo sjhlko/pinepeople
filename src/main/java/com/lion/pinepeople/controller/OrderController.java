8604a07df3adc5066fe32df619f9dbf717375622
ajin0516 <dkwls7432@naver.com>
2023-01-17 15:34:56 +0900

    feat: Order, Alarm Entity 생성

--- src/main/java/com/lion/pinepeople/controller/OrderController.java
+++ src/main/java/com/lion/pinepeople/controller/OrderController.java
@@ -1,4 +1,23 @@
 package com.lion.pinepeople.controller;
 
+import com.lion.pinepeople.domain.response.Response;
+import com.lion.pinepeople.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderservice;
+
+//    @PostMapping("/party/{partyId}/orders")
+//    public Response<OrderResponse> order(@PathVariable Long partyId, OrderRequest orderRequest) {
+//
+//        return Response.success();
+//    }
 }
---
+++ src/main/java/com/lion/pinepeople/domain/entity/Order.java
@@ -0,0 +1,44 @@
+package com.lion.pinepeople.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "orders")
+public class Order{
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @Enumerated(STRING)
+    private OrderType orderType;
+
+    private Integer cost;
+
+    private Integer accumulateCost;
+
+    private LocalDateTime orderDate;
+
+    private Integer discountPoint;
+
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+//
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name = "party_id")
+//    private Party party;
+}
