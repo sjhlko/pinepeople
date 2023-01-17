e69ee2ca78c1674358b86c25ea590eba82a0682f
strongcookdas <goodjav141592@gmail.com>
2023-01-17 15:32:26 +0900

    feat : User 엔티티, 리포지토리 구현

--- src/main/java/com/lion/pinepeople/controller/UserController.java
+++ src/main/java/com/lion/pinepeople/controller/UserController.java
@@ -1,6 +1,26 @@
 package com.lion.pinepeople.controller;
 
+import com.lion.pinepeople.domain.dto.UserJoinRequest;
+import com.lion.pinepeople.domain.dto.UserJoinResponse;
+import com.lion.pinepeople.domain.response.Response;
+import com.lion.pinepeople.service.UserService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Api(tags = "User API")
 public class UserController {
-    //usercontroller
-    //usercontroller
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest){
+        return null;
+    }
 }
