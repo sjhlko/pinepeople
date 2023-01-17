d5b9af580b9f42f86321e5420c4357285c00653c
ajin0516 <dkwls7432@naver.com>
2023-01-17 23:05:05 +0900

    feat: 주문,조회 기능 구현 1. 주문 생성-POST /api/party/{partyId}/orders 2. 주문 단건 조회-GET /api/users-lists/{orderId} 3. 주문 내역 조회-GET /api/users/order-lists - Dto 생성 - OrderType 수정

--- src/main/java/com/lion/pinepeople/domain/entity/Order.java
+++ src/main/java/com/lion/pinepeople/domain/entity/Order.java
@@ -1,7 +1,7 @@
 package com.lion.pinepeople.domain.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.lion.pinepeople.domain.dto.OrderResponse;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -16,10 +16,11 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "orders")
-public class Order{
+@Builder
+@AllArgsConstructor
+public class Order {
 
     @Id
-
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "order_id")
     private Long id;
@@ -35,11 +36,37 @@ public class Order{
 
     private Integer discountPoint;
 
-//    @ManyToOne(fetch = LAZY)
-//    @JoinColumn(name = "user_id")
-//    private User user;
-//
-//    @ManyToOne(fetch = LAZY)
-//    @JoinColumn(name = "party_id")
-//    private Party party;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "party_id")
+    private Party party;
+
+    // 주문 메소드
+    public static Order creatOrder(User user, Party party) {
+        Order order = new Order();
+        return Order.builder()
+                .user(user)
+                .party(party)
+                .orderDate(LocalDateTime.now())
+                .orderType(order.getOrderType())
+                .cost(order.getTotalCost())
+                .accumulateCost(order.getAccumulateCost())
+                .build();
+    }
+
+    // 총 주문 금액 메소드
+    public int getTotalCost() {
+        int totalCost = cost;
+        totalCost -= getDiscountPoint();
+        return totalCost;
+    }
+
+    // 주문 타입(카드 결제, 만나서 결제)
+    public void selectOrderType(OrderType orderType) {
+        this.orderType = orderType;
+    }
+
 }
--- src/main/java/com/lion/pinepeople/service/OrderService.java
+++ src/main/java/com/lion/pinepeople/service/OrderService.java
@@ -1,10 +1,27 @@
 package com.lion.pinepeople.service;
 
 
+import com.lion.pinepeople.domain.dto.OrderRequest;
+import com.lion.pinepeople.domain.dto.OrderResponse;
+import com.lion.pinepeople.domain.dto.OrderSearchResponse;
+import com.lion.pinepeople.domain.entity.Order;
+import com.lion.pinepeople.domain.entity.OrderType;
+import com.lion.pinepeople.domain.entity.Party;
+import com.lion.pinepeople.domain.entity.User;
+import com.lion.pinepeople.exception.ErrorCode;
+import com.lion.pinepeople.exception.customException.AppException;
 import com.lion.pinepeople.repository.OrderRepository;
+import com.lion.pinepeople.repository.PartyCommentRepository;
+import com.lion.pinepeople.repository.PartyRepository;
 import com.lion.pinepeople.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +29,61 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final UserRepository userRepository;
+    private final PartyRepository partyRepository;
 
+    @Transactional
+    public OrderResponse order(Long userId, Long partyId) {
+        // 해당 id의 회원 없음
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, "존재하지 않는 회원입니다"));
 
+        // 해당 id의 파티 없음
+        Party findParty = partyRepository.findById(partyId)
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, "존재하지 않는 방입니다"));
+
+        // 회원의 파티 존재여부 (에러 코드 추가 필요 - 존재하지 않는 파티)
+        Optional<Order> orderParty = orderRepository.findByUserAndParty(findUser, findParty);
+
+        if (OrderType.CREDIT_CARD == orderParty.get().getOrderType()) {
+            orderParty.get().selectOrderType(OrderType.CREDIT_CARD);
+        } else {
+            orderParty.get().selectOrderType(OrderType.MEET_AND_PAY);
+        }
+        // 주문 생성
+        Order createOrder = orderParty.get().creatOrder(findUser, findParty);
+
+        // 주문 저장
+        Order saveOrder = orderRepository.save(createOrder);
+
+        return OrderResponse.builder()
+                .message("주문 완료")
+                .orderType(saveOrder.getOrderType())
+                .orderDate(saveOrder.getOrderDate())
+                .cost(saveOrder.getTotalCost())
+                .discountPoint(saveOrder.getDiscountPoint())
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public OrderSearchResponse findOrder(Long orderId, Long userId) {
+        // 해당 id의 회원 없음
+        userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, "존재하지 않는 회원입니다."));
+
+        // 해당 id의 주문 없음
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, "해당 주문은 존재하지 않습니다."));
+        return OrderSearchResponse.toDto(order);
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderSearchResponse> findAll(Long userId, Pageable pageable) {
+        // 해당 id의 회원 없음
+        userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, "존재하지 않는 회원입니다."));
+        Page<Order> findAll = orderRepository.findAll(pageable);
+        Page<OrderSearchResponse> findOrderAll = OrderSearchResponse.toDtoList(findAll);
+        return findOrderAll;
+    }
 }
