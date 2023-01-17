91c4da1c1a75cafd83381894d86c51799be09c6a
sujin-park0607 <goweg0607@gmail.com>
2023-01-17 17:51:25 +0900

    feat: GET api/users/{userId}/brix 당도 조회 기능 추가
    1. GET controller 추가
    2. 당도 조회 service 추가

--- src/main/java/com/lion/pinepeople/controller/BrixController.java
+++ src/main/java/com/lion/pinepeople/controller/BrixController.java
@@ -12,10 +12,31 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class BrixController {
     private final BrixService brixService;
+
+    /**
+     *
+     * @param userId 당도평가를 진행할 대상 아이디
+     * @param request 별점
+     * @param authentication 토큰 권한
+     * @return 당도 평가 성공 메세지
+     */
     @PostMapping
     public Response<Void> calculationBrix(@RequestParam Long userId, @RequestBody BrixRequest request, Authentication authentication){
         Long loginUserId = Long.parseLong(authentication.getName());
         String result = brixService.calculationBrix(request, userId, loginUserId);
+        return Response.success(result);
+    }
+
+    /**
+     *
+     * @param userId 당도를 조회할 대상 아이디
+     * @param authentication 로그인한 아이디
+     * @return
+     */
+    @GetMapping
+    public Response<Double> getBrix(@RequestParam Long userId, Authentication authentication){
+        Long loginUserId = Long.parseLong(authentication.getName());
+        Double result = brixService.getBrix(loginUserId, userId);
         return Response.success(result);
     }
 }
--- src/main/java/com/lion/pinepeople/service/BrixService.java
+++ src/main/java/com/lion/pinepeople/service/BrixService.java
@@ -32,4 +32,18 @@ public class BrixService {
         brixRepository.save(brix);
         return "당도 측정 완료";
     }
+
+    public Double getBrix(Long loginUserId, Long userId) {
+        //로그인 유저 검사
+        userRepository.findById(loginUserId)
+                .orElseThrow( () -> new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage()));
+        //별점을 줄 유저 확인
+        User user = userRepository.findById(userId)
+                .orElseThrow( () -> new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage()));
+        //해당 유저의 당도 찾기
+        Brix brix = brixRepository.findByUser(user)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage()));
+
+        return brix.getBrixFigure();
+    }
 }
