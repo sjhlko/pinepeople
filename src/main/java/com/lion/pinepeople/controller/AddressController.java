eb4534c50c0c00639dfcc14670837bb2ac63966a
KimGunWoo9595 <medneter@naver.com>
2023-01-19 17:43:19 +0900

    feat : add jococo

--- src/main/java/com/lion/pinepeople/controller/AddressController.java
+++ src/main/java/com/lion/pinepeople/controller/AddressController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AddressController {
 
-        @GetMapping("/")
+        @GetMapping("/homeMap")
         public String home(){
             return "map/homeMap";
         }
 
+
+    @GetMapping("/currentLocation")
+    public String getCurrent() {
+        return "map/currentLocation";
+    }
 }
