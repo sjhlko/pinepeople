0abdeeb9c19a1901a5347729847a12910d6f211f
KimGunWoo9595 <medneter@naver.com>
2023-01-19 09:52:10 +0900

    ref : 기존의 파티 생성시 카테고리 추가 리펙토링

--- src/main/java/com/lion/pinepeople/controller/PartyController.java
+++ src/main/java/com/lion/pinepeople/controller/PartyController.java
@@ -1,5 +1,6 @@
 package com.lion.pinepeople.controller;
 
+import com.lion.pinepeople.domain.dto.party.PartyCategoryRequest;
 import com.lion.pinepeople.domain.dto.party.PartyCreateRequest;
 import com.lion.pinepeople.domain.dto.party.PartyCreateResponse;
 import com.lion.pinepeople.domain.dto.user.role.UserRoleResponse;
@@ -21,6 +22,19 @@ public class PartyController {
     @ApiOperation(value = "파티 생성")
     public Response<PartyCreateResponse> createParty(@RequestBody PartyCreateRequest partyCreateRequest, Authentication authentication) {
         PartyCreateResponse partyCreateResponse = partyService.createParty(partyCreateRequest, authentication.getName());
+        return Response.success(partyCreateResponse);
+    }
+
+    /**
+     * 기존의 파티생성 시 카테고리 선택추가
+     *
+     * @param branch 대분류 카테고리 이름
+     * @param code 소분류 카테고리 코드
+     * **/
+    @PostMapping("/{branch}/{code}")
+    @ApiOperation(value = "파티 생성 카테고리 선택 추가")
+    public Response<PartyCreateResponse> createPartyWithCategory(@PathVariable String branch, @PathVariable String code, @RequestBody PartyCategoryRequest partyCreateRequest, Authentication authentication) {
+        PartyCreateResponse partyCreateResponse = partyService.createPartyWithCategory(branch,code, partyCreateRequest, authentication.getName());
         return Response.success(partyCreateResponse);
     }
 }
--- src/main/java/com/lion/pinepeople/service/PartyService.java
+++ src/main/java/com/lion/pinepeople/service/PartyService.java
@@ -1,16 +1,20 @@
 package com.lion.pinepeople.service;
 
+import com.lion.pinepeople.domain.dto.party.PartyCategoryRequest;
 import com.lion.pinepeople.domain.dto.party.PartyCreateRequest;
 import com.lion.pinepeople.domain.dto.party.PartyCreateResponse;
+import com.lion.pinepeople.domain.entity.Category;
 import com.lion.pinepeople.domain.entity.Participant;
 import com.lion.pinepeople.domain.entity.Party;
 import com.lion.pinepeople.domain.entity.User;
 import com.lion.pinepeople.exception.ErrorCode;
 import com.lion.pinepeople.exception.customException.AppException;
+import com.lion.pinepeople.repository.CategoryRepository;
 import com.lion.pinepeople.repository.PartyRepository;
 import com.lion.pinepeople.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 @RequiredArgsConstructor
@@ -18,11 +22,26 @@ public class PartyService {
     private final PartyRepository partyRepository;
     private final UserRepository userRepository;
     private final ParticipantService participantService;
+    private final CategoryService categoryService;
+    private final CategoryRepository categoryRepository;
 
+
+    /**origin 파티 생성**/
     public PartyCreateResponse createParty(PartyCreateRequest partyCreateRequest, String userId) {
         User user = userRepository.findById(Long.parseLong(userId))
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage()));
         Party party = partyRepository.save(partyCreateRequest.toEntity());
+        Participant participant = participantService.createHostParticipant(user,party);
+        return PartyCreateResponse.of(party,participant);
+    }
+
+    /**파티 생성시 카테고리 선택 추가**/
+    public PartyCreateResponse createPartyWithCategory(String branch, String code, PartyCategoryRequest request, String userId) {
+        User user = userRepository.findById(Long.parseLong(userId))
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage()));
+        //카테고리 생성
+        Category category = categoryRepository.findByBranchAndCode(branch, code).orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+        Party party = partyRepository.save(request.toEntity(category));
         Participant participant = participantService.createHostParticipant(user,party);
         return PartyCreateResponse.of(party,participant);
     }
