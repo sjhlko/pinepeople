d190e81ed45fcb682caa68d542d9ec464329b328
KimGunWoo9595 <medneter@naver.com>
2023-01-20 12:18:50 +0900

    feat : 파티원 모집 댓글 등록 실패 테스트

--- src/test/java/com/lion/pinepeople/service/PartyCommentServiceTest.java
+++ src/test/java/com/lion/pinepeople/service/PartyCommentServiceTest.java
@@ -4,6 +4,8 @@ import com.lion.pinepeople.domain.dto.partyComment.PartyCommentResponse;
 import com.lion.pinepeople.domain.entity.Party;
 import com.lion.pinepeople.domain.entity.PartyComment;
 import com.lion.pinepeople.domain.entity.User;
+import com.lion.pinepeople.exception.ErrorCode;
+import com.lion.pinepeople.exception.customException.AppException;
 import com.lion.pinepeople.fixture.AllFixture;
 import com.lion.pinepeople.fixture.PartyCommentFixture;
 import com.lion.pinepeople.fixture.PartyFixture;
@@ -21,9 +23,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -58,8 +62,59 @@ class PartyCommentServiceTest {
         /*서비스 로직*/
         PartyCommentResponse response
                 = PartyCommentService.addPartyComment(all.getPartyId(), String.valueOf(all.getUserId()), all.getBody());
-        Assertions.assertThat(response.getPartyId()).isEqualTo(party.getId());
+
+        assertThat(response.getPartyId()).isEqualTo(party.getId());
+        assertThat(response.getBody()).isEqualTo(comment.getBody());
+        assertThat(response.getPartyCommentId()).isEqualTo(comment.getId());
+
+        assertDoesNotThrow(()->response);
 
     }
 
+    @Test
+    @WithMockUser
+    @DisplayName("댓글 등록 실패 : 유저 존재 하지 않음")
+    void 댓글_등록_실패1() throws Exception {
+
+        AllFixture all = AllFixture.getDto();
+        User user = UserFixture.get(all.getEmail(), all.getPassword());
+        Party party = PartyFixture.get(user);
+        PartyComment comment = PartyCommentFixture.get(user, party);
+
+
+        /**회원이 존재하지 않는 상황 가정**/
+        when(userRepository.findById(any())).thenReturn(Optional.empty());
+        when(partyRepository.findById(any())).thenReturn(Optional.of(party));
+        when(PartyCommentRepository.save(any())).thenReturn(comment);
+        /*서비스 로직*/
+        AppException appException = assertThrows(AppException.class,
+                () -> PartyCommentService.addPartyComment(all.getPartyId(), String.valueOf(all.getUserId()), all.getBody()));
+
+        assertThat(appException.getErrorCode()).isEqualTo(ErrorCode.USER_NOT_FOUND);
+
+    }
+
+
+    @Test
+    @WithMockUser
+    @DisplayName("댓글 등록 실패 : 파티 존재 하지 않음")
+    void 댓글_등록_실패2() throws Exception {
+
+        AllFixture all = AllFixture.getDto();
+        User user = UserFixture.get(all.getEmail(), all.getPassword());
+        Party party = PartyFixture.get(user);
+        PartyComment comment = PartyCommentFixture.get(user, party);
+
+
+        /**회원이 존재하지 않는 상황 가정**/
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+        when(partyRepository.findById(any())).thenReturn(Optional.empty());
+        when(PartyCommentRepository.save(any())).thenReturn(comment);
+        /*서비스 로직*/
+        AppException appException = assertThrows(AppException.class,
+                () -> PartyCommentService.addPartyComment(all.getPartyId(), String.valueOf(all.getUserId()), all.getBody()));
+
+        assertThat(appException.getErrorCode()).isEqualTo(ErrorCode.PARTY_NOT_FOUND);
+
+    }
 }
