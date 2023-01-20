1861fbb4413ad26932408006a5b3711d80238277
ajin0516 <dkwls7432@naver.com>
2023-01-20 19:03:46 +0900

    feat: PaymentType(카드결제, 만나서 결제), OrderStatus(주문완료, 주문취소) enum 파일 수정 및 추가

---
+++ src/main/java/com/lion/pinepeople/domain/entity/PaymentType.java
@@ -0,0 +1,12 @@
+package com.lion.pinepeople.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PaymentType {
+
+    CREDIT_CART,
+    CONTACT_PAYMENT
+}
