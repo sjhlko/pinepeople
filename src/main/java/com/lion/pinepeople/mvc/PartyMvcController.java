4cf1c13ebc7551f5340a7f51c9cacaab77028688
KimGunWoo9595 <medneter@naver.com>
2023-01-30 15:23:47 +0900

    fear : post Mvc Controller

--- src/main/java/com/lion/pinepeople/mvc/PartyMvcController.java
+++ src/main/java/com/lion/pinepeople/mvc/PartyMvcController.java
@@ -28,6 +28,7 @@ public class PartyMvcController {
     private final PartyService partyService;
     private final PartyRepository partyRepository;
 
+    /**파티 리스트 페이지**/
     @GetMapping("/party/list")
     public String getPartyList(@PageableDefault(page = 0, size = 5, sort = "createdAt",
             direction = Sort.Direction.DESC) Pageable pageable, Model model, String address, String partyContent, String partyTitle) {
@@ -40,15 +41,16 @@ public class PartyMvcController {
             partys = partyService.searchParty(pageable, address, partyContent, partyTitle);
             model.addAttribute("partys", partys);
         }
-        doCategory(model);
+        //카테고리 검색🔽
+        categoryService.doCategory(model);
         doPage(model, partys);
         return "party/partyList";
     }
 
 
-
+    /**파티 상세보기**/
     @GetMapping("/party/detail/{id}")
-    public String getPartyList(@PathVariable Long id, Model model, String title) {
+    public String getPartyList(@PathVariable Long id, Model model) {
         log.info("id:{}", id);
         PartyInfoResponse party = partyService.getParty(id);
         model.addAttribute("party", party);
@@ -59,31 +61,29 @@ public class PartyMvcController {
     @GetMapping("/party/category/{name}")
     public String getCategoryParties(@PathVariable String name,Model model,@PageableDefault(page = 0, size = 5, sort = "createdAt",
             direction = Sort.Direction.DESC) Pageable pageable) {
+
         Page<Party> parties = partyRepository.findByCategory_Name(pageable, name);
         Page<PartyInfoResponse> partys = PartyInfoResponse.toPage(parties);
         model.addAttribute("partys", partys);
-        doCategory(model);
+        categoryService.doCategory(model);
         doPage(model, partys);
         return "party/partyList";
+
     }
 
 
+    /**페이징 정보 model로 넘기는 메서드**/
     private void doPage(Model model, Page<PartyInfoResponse> partys) {
         /**페이징 처리**/
         int nowPage = partys.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
         int endPage = Math.min(nowPage + 9, partys.getTotalPages());
-        /**페이지 최신순 플래그🔽**/
+        /**페이지 **/
         model.addAttribute("nowPage", nowPage);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
     }
 
-    private void doCategory(Model model) {
-        List<Category> rightNows = categoryService.getCategorySteadily("RightNow!", 1);
-        List<Category> steadilys = categoryService.getCategorySteadily("Steadily!", 1);
-        model.addAttribute("rightNows", rightNows);
-        model.addAttribute("steadilys", steadilys);
-    }
+
 
 }
