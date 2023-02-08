74e18c48be98b4e794b3cd5ae886b2c29926616d
GRAM\Kim Jinah <k0605ja2@naver.com>
2023-02-08 09:41:46 +0900

    feat: 메소드 추가

--- src/main/java/com/lion/pinepeople/repository/PostRepository.java
+++ src/main/java/com/lion/pinepeople/repository/PostRepository.java
@@ -5,10 +5,20 @@ import com.lion.pinepeople.domain.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findByUser (Pageable pageable, User user);
+
+    @Modifying
+    @Query("update Post post set post.hits = post.hits + 1 where post.id = :id")
+    Long countUpHits(Long id);
+
+    // 검색
+    Page<Post> findByTitle(String keyword, Pageable pageable);
+    Page<Post> findByKeywordContaining(String keyword, Pageable pageable);
 
 
 }
