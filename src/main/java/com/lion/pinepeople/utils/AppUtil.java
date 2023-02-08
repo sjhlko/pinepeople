23fe32876a941f1bc6796595a54565b38a5c99de
GRAM\Kim Jinah <k0605ja2@naver.com>
2023-02-08 09:40:18 +0900

    refactor: 전역 의존성 없는 메소드 삭제

--- src/main/java/com/lion/pinepeople/utils/AppUtil.java
+++ src/main/java/com/lion/pinepeople/utils/AppUtil.java
@@ -1,55 +1,35 @@
 package com.lion.pinepeople.utils;
 
 
-import com.lion.pinepeople.domain.entity.Comment;
-import com.lion.pinepeople.domain.entity.Post;
-import com.lion.pinepeople.domain.entity.User;
 import com.lion.pinepeople.exception.ErrorCode;
 import com.lion.pinepeople.exception.customException.AppException;
 import com.lion.pinepeople.repository.CommentRepository;
 import com.lion.pinepeople.repository.PostRepository;
 import com.lion.pinepeople.repository.UserRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
 public class AppUtil {
 
-    /****
-     * findUser 회원 이름 확인
-     * @param userRepository
-     * @param id
-     * @return
-     */
-    public static User findUser(UserRepository userRepository, Long id) {
-        return userRepository.findById(id).orElseThrow(() -> {
-            throw new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage());
-        });
-    }
-
-    /***
-     * findPost 게시물 확인
-     * @param postRepository
-     * @param id
-     * @return
-     */
-    public static Post findPost(PostRepository postRepository, Long id) {
-        return postRepository.findById(id).orElseThrow(() -> {
-            throw new AppException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage());
-        });
-
-    }
-
+    private static UserRepository userRepository;
+    private static PostRepository postRepository;
+    private static CommentRepository commentRepository;
 
     // 게시물 작성 본인 여부 확인
-    public static void checkUser(Long checkUserId, Long id) {
-        if (!checkUserId.equals(id)) {
+    public static void verifyPostAuthor(String userId, Long postId) {
+        if (userRepository.findById(Long.parseLong(userId)).get().getId() != postRepository.findById(postId).get().getUser().getId()) {
             throw new AppException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
         }
     }
 
-    // 댓글 확인
-    public static Comment findBody(CommentRepository commentRepository, Long postId, Long id) {
-        return commentRepository.findByPostIdAndId(postId, id).orElseThrow(() -> {
-            throw new AppException(ErrorCode.COMMENT_NOT_FOUND, ErrorCode.COMMENT_NOT_FOUND.getMessage());
-        });
+
+    // 댓글 작성 본인 여부 확인
+    public static void verifyCommentAuthor(String userId, Long commentId) {
+        if (userRepository.findById(Long.parseLong(userId)).get().getId() != commentRepository.findById(commentId).get().getUser().getId()) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
+        }
     }
 
 }
