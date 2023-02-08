2a6220eba5c465c028961ead5f982057680abc44
GRAM\Kim Jinah <k0605ja2@naver.com>
2023-02-08 09:42:23 +0900

    refactor: 메소드 추가

--- src/main/java/com/lion/pinepeople/service/CommentService.java
+++ src/main/java/com/lion/pinepeople/service/CommentService.java
@@ -1,12 +1,11 @@
 package com.lion.pinepeople.service;
 
-import com.lion.pinepeople.domain.dto.comment.CommentDeleteResponse;
-import com.lion.pinepeople.domain.dto.comment.CommentResponse;
-import com.lion.pinepeople.domain.dto.comment.CommentUpdateResponse;
-import com.lion.pinepeople.domain.dto.post.PostReadResponse;
+import com.lion.pinepeople.domain.dto.comment.*;
 import com.lion.pinepeople.domain.entity.Comment;
 import com.lion.pinepeople.domain.entity.Post;
 import com.lion.pinepeople.domain.entity.User;
+import com.lion.pinepeople.exception.ErrorCode;
+import com.lion.pinepeople.exception.customException.AppException;
 import com.lion.pinepeople.repository.CommentRepository;
 import com.lion.pinepeople.repository.PostRepository;
 import com.lion.pinepeople.repository.UserRepository;
@@ -15,11 +14,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
+
 
 
     private final UserRepository userRepository;
@@ -27,73 +29,78 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
 
-    // response 들어온 걸 파라미터로 받으니까 entity -> dto로 바꾸고 로직 처리?
-    public CommentResponse createComment(String userId, Long id, String comment) {
 
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(userId)); // 회원 확인
-        Post findPost = AppUtil.findPost(postRepository, id); // 게시물 확인
+    @Transactional
+    public CommentCreateResponse createComment(String userId, Long postId, CommentCreateRequest commentCreateRequest) {
 
-        Comment saveComment = Comment.convertToEntity(comment, findUser, findPost); // 댓글 Dto -> Entity 변환
-        saveComment = commentRepository.save(saveComment); // 댓글 Entity 저장
+        Comment savedComment = commentRepository.save(commentCreateRequest.of(validateUser(userId), validatePost(postId)));
 
-        return CommentResponse.convertToDto(saveComment); // 저장한 entity -> Dto 리턴 => Copntroller
-
-    }
-
-
-    public Page<CommentResponse> readCommentPage(Pageable pageable, Long postId) {
-        Post findPost = AppUtil.findPost(postRepository, postId);
-        Page<Comment> commentPage = commentRepository.findByPost(pageable, findPost);
-        return CommentResponse.convertListToDto(commentPage);
+        return CommentCreateResponse.of(savedComment);
     }
 
 
 
-    public Page<CommentResponse> readMyComments(Pageable pageable, String userId) {
+    public Page<CommentReadResponse> readCommentPage(Pageable pageable, Long postId) {
 
-        // 회원 확인
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(userId));
+        Page<Comment> comments = commentRepository.findByPost(pageable, validatePost(postId));
 
-        // 내가 쓴 댓글 불러오기
-        Page<Comment> myComments = commentRepository.findByUser(pageable, findUser);
-
-        return CommentResponse.convertListToDto(myComments);
+        return CommentReadResponse.of(comments);
     }
 
 
 
+    public Page<CommentReadResponse> getMyComments(String userId, Pageable pageable) {
 
-    public CommentUpdateResponse update(String userId, Long postId, Long id, String comment) {
-        //유저체크
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(userId));
-        //포스트 체크
-        Post findPost = AppUtil.findPost(postRepository, postId);
-        //댓글 체크
-        Comment findBody = AppUtil.findBody(commentRepository, postId, id);
-        //작성자 체크
-        AppUtil.checkUser(findBody.getUser().getId(), findUser.getId());
-        //댓글 수정
-        findBody.update(comment);
-        commentRepository.saveAndFlush(findBody);
-        return CommentUpdateResponse.convertToDto(findBody);
+        Page<Comment> myComments = commentRepository.findByUser(pageable, validateUser(userId));
+
+        return CommentReadResponse.of(myComments);
     }
 
 
-    public CommentDeleteResponse delete(String userId, Long postId, Long id) {
-        //유저 체크
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(userId));
-        //포스트 체크
-        Post findPost = AppUtil.findPost(postRepository, postId);
-        //댓글 체크
-        Comment findBody = AppUtil.findBody(commentRepository, postId, id);
-        //작성자 비교
-        AppUtil.checkUser(findBody.getUser().getId(), findUser.getId());
-        //삭제
-        commentRepository.delete(findBody);
-        //DTO 리턴
-        return CommentDeleteResponse.ConvertToDto("댓글 삭제 완료", findBody.getId());
+    public CommentUpdateResponse updateComment(String userId, Long postId, Long commentId, String body) {
+
+        validateUser(userId);
+        validateComment(postId, commentId);
+        AppUtil.verifyCommentAuthor(userId, commentId);
+
+        Comment updatedComment = validateComment(postId, commentId);
+
+        updatedComment.updateComment(body);
+
+        return CommentUpdateResponse.of(updatedComment);
     }
 
+
+    public CommentDeleteResponse deleteComment(String userId, Long postId, Long commentId) {
+
+        validateUser(userId);
+        validateComment(postId, commentId);
+        AppUtil.verifyCommentAuthor(userId, commentId);
+
+        commentRepository.delete(validateComment(postId, commentId));
+
+        return CommentDeleteResponse.of(commentId);
+    }
+
+
+    public User validateUser(String userId) {
+        return userRepository.findById(Long.parseLong(userId)).orElseThrow(() -> {
+            throw new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage());
+        });
+    }
+
+    public Post validatePost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() -> {
+            throw new AppException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage());
+        });
+    }
+
+
+    public Comment validateComment(Long postId, Long commentId) {
+        return commentRepository.findByPostIdAndId(postId, commentId).orElseThrow(() -> {
+            throw new AppException(ErrorCode.COMMENT_NOT_FOUND, ErrorCode.COMMENT_NOT_FOUND.getMessage());
+        });
+    }
 
 
 }
--- src/main/java/com/lion/pinepeople/service/PostService.java
+++ src/main/java/com/lion/pinepeople/service/PostService.java
@@ -1,8 +1,8 @@
 package com.lion.pinepeople.service;
 
 
-import com.lion.pinepeople.domain.dto.post.PostReadResponse;
-import com.lion.pinepeople.domain.dto.post.PostResponse;
+import com.lion.pinepeople.domain.dto.post.*;
+import com.lion.pinepeople.domain.entity.Comment;
 import com.lion.pinepeople.domain.entity.Post;
 import com.lion.pinepeople.domain.entity.User;
 import com.lion.pinepeople.exception.ErrorCode;
@@ -16,136 +16,130 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @Service
 @RequiredArgsConstructor
-@Transactional // 트랜젝션
+@Transactional(readOnly = true)
 @Slf4j
 public class PostService {
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
+    @Transactional
+    public PostCreateResponse create(PostCreateRequest postCreateRequest, String userId) {
 
-    /****
-     * create 게시물 작성
-     * @param id
-     * @param title
-     * @param body
-     * @return PostResponse(DTO)
-     */
-    public PostResponse create(String id, String title, String body) {
+        validateUser(userId);
 
-        // 회원 아이디(인증) 확인
-        log.info("id:{}", id);
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(id)); // Long 형변환
+        Post savedPost = postRepository.save(postCreateRequest.of());
 
-        // 게시물 저장
-        Post savePost = Post.convertToEntity(title, body, findUser);
-        savePost = postRepository.save(savePost);
-
-        // Entity -> DTO 변환하여 반환
-        return PostResponse.convertToDto("포스트 등록 성공", savePost.getId());
+        return PostCreateResponse.of(savedPost);
     }
 
 
-    /****
-     * getPost 게시물 단건 조회
-     * @param id
-     * @return
-     */
-    public PostReadResponse getPost(Long id) {
+    public PostReadResponse getPost(Long postId) {
 
-        // 게시물 id로 게시물 검색
-        Post findPost = AppUtil.findPost(postRepository, id);
+        validatePost(postId);
+        countUpHits(postId);
 
-        // Post entity -> DTO로 변환 후 반환
-        return PostReadResponse.convertToDto(findPost);
+        return PostReadResponse.of(validatePost(postId));
 
     }
 
 
-    /***
-     * getPostList 게시물 목록 조회
-     * @param pageable
-     * @return posts(Dto)
-     */
     public Page<PostReadResponse> getPostList(Pageable pageable) {
 
-        Page<Post> posts = postRepository.findAll(pageable);
-        return PostReadResponse.convertListToDto(posts);
+        return PostReadResponse.of(postRepository.findAll(pageable));
 
     }
 
 
-    /***
-     * getMyPosts 내 게시물 조회
-     * @param pageable
-     * @param id
-     * @return
-     */
-    public Page<PostReadResponse> getMyPosts(Pageable pageable, Long id) {
+    public Page<PostReadResponse> getMyPosts(Pageable pageable, String userId) {
 
-        // 회원 확인
-        User findUser = AppUtil.findUser(userRepository, id);
-
-        // 내가 쓴 게시물 불러오기
-        Page<Post> myPosts = postRepository.findByUser(pageable, findUser);
-
-        return PostReadResponse.convertListToDto(myPosts);
-    }
-
-
-    /***
-     * update 게시물 수정
-     * @param postId
-     * @param userId
-     * @param title
-     * @param body
-     * @return
-     */
-    public PostResponse update(Long postId, String userId, String title, String body) {
-
-        // 게시물 아이디 확인
-        log.info("postId:{}", postId);
-        Post updatePost = AppUtil.findPost(postRepository, postId);
-
-        //회원 아이디(인증) 확인
-        log.info("id:{}", userId);
-        User findByUser = AppUtil.findUser(userRepository, Long.parseLong(userId));
-
-        User findUser = userRepository.findById(Long.parseLong(userId)).orElseThrow(() -> {
-            throw new AppException(ErrorCode.USER_NOT_FOUND, "해당 회원이 없습니다.");
-                });
-
-
-        // 게시물 작성자 비교
-        AppUtil.checkUser(updatePost.getUser().getId(), findUser.getId());
-
-
-        updatePost.update(title, body);
-        return PostResponse.convertToDto("게시물 수정 완료", updatePost.getId());
+        return PostReadResponse.of(postRepository.findByUser(pageable, validateUser(userId)));
 
     }
 
 
+    @Transactional
+    public PostUpdateResponse update(Long postId, String userId, PostUpdateRequest postUpdateRequest) {
 
-    /***
-     * 게시물 삭제
-     * @param postId
-     * @param userId
-     * @return
-     */
-    public PostResponse delete(Long postId, String userId) {
+        validatePost(postId);
+        validateUser(userId);
+        AppUtil.verifyPostAuthor(userId, postId);
 
-        Post deletePost = AppUtil.findPost(postRepository, postId);
-        User findUser = AppUtil.findUser(userRepository, Long.parseLong(userId));
-        AppUtil.checkUser(deletePost.getUser().getId(), findUser.getId()); // 메소드 이름 수정
+        Post updatedPost = validatePost(postId);
 
-        postRepository.delete(deletePost);
-        return PostResponse.convertToDto("게시물 삭제 완료", deletePost.getId());
+        updatedPost.updatePost(postUpdateRequest.getTitle(), postUpdateRequest.getBody());
 
+
+        return PostUpdateResponse.of(updatedPost);
+    }
+
+
+    public PostDeleteResponse delete(Long postId, String userId) {
+
+        validatePost(postId);
+        validateUser(userId);
+        AppUtil.verifyPostAuthor(userId, postId);
+
+        postRepository.delete(validatePost(postId));
+
+        return PostDeleteResponse.of(validatePost(postId));
+
+    }
+
+
+    // 키워드로 게시물 검색
+    public Page<PostReadResponse> searchByKeyword(Pageable pageable, String keyword) {
+
+
+        if (keyword == null) {
+            searchByKeyword(pageable, null);
+        } else {
+            searchByKeyword(pageable, keyword);
+        }
+
+        return PostReadResponse.of(postRepository.findByKeywordContaining(keyword, pageable));
+
+
+    }
+
+
+    // 제목으로 게시물 검색
+    public Page<PostReadResponse> searchByTitle(Pageable pageable, String keyword) {
+
+
+        if (keyword == null) {
+            searchByTitle(pageable, null);
+        } else {
+            searchByTitle(pageable, keyword);
+        }
+
+        return PostReadResponse.of(postRepository.findByTitle(keyword, pageable));
+
+    }
+
+
+    public User validateUser(String userId) {
+        return userRepository.findById(Long.parseLong(userId)).orElseThrow(() -> {
+            throw new AppException(ErrorCode.USER_NOT_FOUND, ErrorCode.USER_NOT_FOUND.getMessage());
+        });
+    }
+
+    public Post validatePost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() -> {
+            throw new AppException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage());
+        });
+    }
+
+
+    public Long countUpHits(Long id) {
+        return postRepository.countUpHits(id);
     }
 
 
