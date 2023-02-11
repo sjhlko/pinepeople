5b85817140d3ba608d1dbcada07c361ef5434425
GRAM\Kim Jinah <k0605ja2@naver.com>
2023-02-11 21:59:52 +0900

    refactor : of() 메소드 추가

--- src/main/java/com/lion/pinepeople/domain/dto/comment/CommentCreateResponse.java
+++ src/main/java/com/lion/pinepeople/domain/dto/comment/CommentCreateResponse.java
@@ -12,13 +12,16 @@ import lombok.*;
 public class CommentCreateResponse {
 
 
-
     private Long id;
+    private String body;
+    private String userName;
 
 
-    public static CommentCreateResponse of (Comment savedComment) {
+    public static CommentCreateResponse of(Comment savedComment) {
         return CommentCreateResponse.builder()
                 .id(savedComment.getId())
+                .body(savedComment.getBody())
+                .userName(savedComment.getUser().getName())
                 .build();
     }
 
