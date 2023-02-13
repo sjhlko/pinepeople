f7d6d30b7dd122f53f6cfdb2fc18b2267f3f6d1d
sjhlko444 <sjhlko444@gmail.com>
2023-02-13 15:30:30 +0900

    feat: 채팅 관련 entity 추가

---
+++ src/main/java/com/lion/pinepeople/domain/entity/Chat.java
@@ -0,0 +1,32 @@
+package com.lion.pinepeople.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE chat SET deleted_at = current_timestamp WHERE chat_id = ? ")
+public class Chat extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long id;
+    private String message;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User sender;
+
+
+}
